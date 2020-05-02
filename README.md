8dc537c42ec20adf9149ab3e7370b63781cc6c26
erwin kurniawan <erwin@sleekr.co>
2020-05-02 22:17:21 +0700

    serve

--- README.md
+++ README.md
@@ -8,10 +8,11 @@ Bootstrap Template is simple thing to use bootstrap for your frontend environmen
 npm install
 ```
 
-## Watch localhost:9000
+## Serve to browser
 ```bash
 npm run watch
 ```
+Your browser automatically open http://localhost:9000/
 
 ## Contributing
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,7 @@ import gulp from 'gulp';
 import sass from 'gulp-sass';
 import plugins from 'gulp-load-plugins';
 import del from 'del';
+import browserSync from 'browser-sync';
 
 const $ = plugins();
 
@@ -14,13 +15,31 @@ export const styles = () => {
     .pipe($.sass.sync().on('error', sass.logError))
     .pipe($.cssmin())
     .pipe($.rename({suffix: '.min'}))
-    .pipe(gulp.dest('build/styles'));
+    .pipe(gulp.dest('build/styles'))
+    .pipe(browserSync.stream());
 };
+
+// serve
+const run = () => browserSync.init({
+  port: '9000',
+  server: {
+    baseDir: 'build',
+    serveStaticOptions: {
+      extensions: ['html', 'js'],
+    },
+  },
+  notify: true,
+});
 
 // watch
 export const watch = () =>  {
-  return gulp.watch(dir.styles.src, styles);
+  return gulp.watch('src/styles/**/*.scss', gulp.series(styles));
 }
+
+// serve
+export const serve = (done) =>  {
+  gulp.series(clean, styles, gulp.parallel(run, watch))(done);
+};
 
 // build
 export const build = (done) => gulp.series(clean, gulp.parallel(styles))(done);
